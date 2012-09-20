2512581423b943ebae5b5a43f9176afd2f4f8253
Huy <nqhuy91bk@gmail.com>
2012-09-20 16:59:00 +0700

    abc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookProject::Application.config.secret_token = '9c02c2b7c5a34c8309c3d13f50ed31e023a937755fac5226f3fc316b10e87c2b1826ad9402135c15ab177eb26a6968a5d2bc9bd52cd7f710ef72a6f55b5fcb88'
